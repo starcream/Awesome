c8e5ebdc98a536f56254494cda5db79793d8adf6
King Yizhi <13121227218@163.com>
2020-08-20 09:26:57 +0800

    Create Jump_Game2.cpp

---
+++ 数据结构/贪心/Jump_Game2.cpp
@@ -0,0 +1,51 @@
+Leetcode 45
+Given an array of non-negative integers, you are initially positioned at the first index of the array.
+
+Each element in the array represents your maximum jump length at that position.
+
+Your goal is to reach the last index in the minimum number of jumps.
+
+Example:
+
+Input: [2,3,1,1,4]
+Output: 2
+Explanation: The minimum number of jumps to reach the last index is 2.
+    Jump 1 step from index 0 to 1, then 3 steps to the last index
+    
+    
+    
+这题如果用dp，效率会比较低
+正解是用贪心，每次选择能跳到的最远的点
+    假设   3 5 4都能跳到 那么肯定选择5和4，因为其覆盖了3下次的射程
+  
+public int jump(int[] nums) {
+    int end = 0;
+    int maxPosition = 0; 
+    int steps = 0;
+    for(int i = 0; i < nums.length - 1; i++){
+        //找能跳的最远的
+        maxPosition = Math.max(maxPosition, nums[i] + i); 
+        if( i == end){ //遇到边界，就更新边界，并且步数加一
+            end = maxPosition;
+            steps++;
+        }
+    }
+    return steps;
+}
+
+
+或者用BFS的思路，第i步能到达的点都摆在树的第i层
+ int jump(int A[], int n) {
+	 if(n<2)return 0;
+	 int level=0,currentMax=0,i=0,nextMax=0;
+
+	 while(currentMax-i+1>0){		//nodes count of current level>0
+		 level++;
+		 for(;i<=currentMax;i++){	//traverse current level , and update the max reach of next level
+			nextMax=max(nextMax,A[i]+i);
+			if(nextMax>=n-1)return level;   // if last element is in level+1,  then the min jump=level 
+		 }
+		 currentMax=nextMax;
+	 }
+	 return 0;
+ }
