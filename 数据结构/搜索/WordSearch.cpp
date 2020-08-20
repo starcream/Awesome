68b69069a5dab916c1d4f97d2f0b938cb2e1c523
King Yizhi <13121227218@163.com>
2020-08-20 16:03:43 +0800

    Create WordSearch.cpp

---
+++ 数据结构/搜索/WordSearch.cpp
@@ -0,0 +1,59 @@
+LC 79 Word Search
+
+Given a 2D board and a word, find if the word exists in the grid.
+
+The word can be constructed from letters of sequentially adjacent cell, where "adjacent" cells are those horizontally or vertically neighboring. The same letter cell may not be used more than once.
+
+Example:
+
+board =
+[
+  ['A','B','C','E'],
+  ['S','F','C','S'],
+  ['A','D','E','E']
+]
+
+Given word = "ABCCED", return true.
+Given word = "SEE", return true.
+Given word = "ABCB", return false.
+--------------------------------------------
+显然是用dfs，找到起始点分多条线进行dfs
+但是它不允许走重复点，所以需要记录
+如果用vector或者set记录，费时费力
+使用board本身来记录
+如果每次都是新board，费时费空间
+如果直接在原board上修改，会出现错误
+所以最好的方法是，直接在原board上修改，然后在当前dfs结束后，修复之前修改的值
+注意，dfs时一定要使用||符号，这样只要有一个为1，后面的dfs就不用进行
+
+class Solution {
+public:
+    bool exist(vector<vector<char>>& board, string word) {
+        if (word.empty()) return true;
+        if (board.empty() || board[0].empty()) return false;
+        
+        m = board.size();
+        n = board[0].size();
+        for (int i = 0; i < m; ++i)
+            for (int j = 0; j < n; ++j)
+                // traverse the board to find the first char
+                if (dfsSearch(board, word, 0, i, j)) return true;
+        return false;
+    }
+private:
+    int m;
+    int n;
+    bool dfsSearch(vector<vector<char>>& board, string& word, int k, int i, int j) {
+        if (i < 0 || i >= m || j < 0 || j >= n || word[k] != board[i][j]) return false;
+        if (k == word.length() - 1) return true;  // found the last char
+
+        char cur = board[i][j];
+        board[i][j] = '*';  // used
+        bool search_next = dfsSearch(board, word, k+1, i-1 ,j) 
+                        || dfsSearch(board, word, k+1, i+1, j) 
+                        || dfsSearch(board, word, k+1, i, j-1)
+                        || dfsSearch(board, word, k+1, i, j+1);
+        board[i][j] = cur;  // reset,非常的聪明！
+        return search_next;
+    }
+};
