1ebeff091122cde73cdf9325153c43caae8a248e
King Yizhi <13121227218@163.com>
2020-08-21 10:51:15 +0800

    Create 中位数.cpp

---
+++ 数据结构/堆&栈/中位数.cpp
@@ -0,0 +1,46 @@
+LC 295. Find Median from Data Stream
+Median is the middle value in an ordered integer list. If the size of the list is even, there is no middle value. So the median is the mean of the two middle value.
+
+For example,
+[2,3,4], the median is 3
+
+[2,3], the median is (2 + 3) / 2 = 2.5
+
+Design a data structure that supports the following two operations:
+
+void addNum(int num) - Add a integer number from the data stream to the data structure.
+double findMedian() - Return the median of all elements so far.
+
+我自己用的multiset和一个移动的iterator，但是multiset可能比较占空间？
+这个解法肯定是okay的。两个堆（优先队列），一边存一半，一边存小的半边，维护一个最大值；一边存大的半边，维护一个最小值
+两个堆，两个栈都可以起到一些意想不到的效果
+------------------------------------
+class MedianFinder {
+private:
+    priority_queue<int> firstQ; // max_heap for the first half
+    priority_queue<int, std::vector<int>, std::greater<int> > secQ; // min_heap for the second half
+public:
+    // Adds a number into the data structure.
+    void addNum(int num) {
+        if(firstQ.empty() || (firstQ.top()>num)) firstQ.push(num); // if it belongs to the smaller half
+        else secQ.push(num); 
+        
+        // rebalance the two halfs to make sure the length difference is no larger than 1
+        if(firstQ.size() > (secQ.size()+1))
+        {
+            secQ.push(firstQ.top());
+            firstQ.pop();
+        }
+        else if(firstQ.size()+1<secQ.size())
+        {
+            firstQ.push(secQ.top());
+            secQ.pop();
+        }
+    }
+
+    // Returns the median of current data stream
+    double findMedian() {
+        if(firstQ.size() == secQ.size()) return firstQ.empty()?0:( (firstQ.top()+secQ.top())/2.0);
+        else return (firstQ.size() > secQ.size())? firstQ.top():secQ.top(); 
+    }
+};
